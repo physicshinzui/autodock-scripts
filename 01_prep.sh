2e434b30d99f2ecf468c289ab3edf3c5617ea1b5
siida <physicshinzui@gmail.com>
2023-02-06 19:20:24 +0900

    Added

--- 01_prep.sh
+++ 01_prep.sh
@@ -6,7 +6,7 @@ target_dirs=`ls $INPUTS`
 for target_dir in $target_dirs; do
     echo "Target dir: $target_dir"
     
-    recepter=`basename $INPUTS/${target_dir}/*.pdb`
+    recepter=`basename $INPUTS/${target_dir}/*_origin.pdb`
     ligand=`basename $INPUTS/${target_dir}/*.mol2`
 
     prepare_receptor -r $INPUTS/$target_dir/$recepter -o pdbqt/${target_dir}_receptor.pdbqt
@@ -15,7 +15,7 @@ for target_dir in $target_dirs; do
     #    The script preapre_ligand does recognise only files in the same working directory.
     #    So, I make a link to an input and put it on the same directory where the script is executed.
     ln -s $INPUTS/$target_dir/$ligand $ligand 
-    prepare_ligand -l $INPUTS/$target_dir/$ligand   -o pdbqt/${target_dir}_ligand.pdbqt
+    prepare_ligand -l $INPUTS/$target_dir/$ligand -o pdbqt/${target_dir}_ligand.pdbqt
 
     rm $ligand # Remove the symbolic link.
 done
