06b71f652d93cfb380e9a36ecb75a108493a5612
siida <physicshinzui@gmail.com>
2023-02-06 14:14:07 +0900

    modify

--- 01_prep.sh
+++ 01_prep.sh
@@ -1,7 +1,21 @@
 #!/bin/bash 
 set -eu 
 
-receptor=$1
-ligand=$2
-prepare_receptor -r $receptor -o receptor.pdbqt
-prepare_ligand -l $ligand -o ligand.pdbqt
+INPUTS='inputs'
+target_dirs=`ls $INPUTS`
+for target_dir in $target_dirs; do
+    echo "Target dir: $target_dir"
+    
+    recepter=`basename $INPUTS/${target_dir}/*.pdb`
+    ligand=`basename $INPUTS/${target_dir}/*.mol2`
+
+    prepare_receptor -r $INPUTS/$target_dir/$recepter -o pdbqt/${target_dir}_receptor.pdbqt
+
+    # NOTE: 
+    #    The script preapre_ligand does recognise only files in the same working directory.
+    #    So, I make a link to an input and put it on the same directory where the script is executed.
+    ln -s $INPUTS/$target_dir/$ligand $ligand 
+    prepare_ligand -l $INPUTS/$target_dir/$ligand   -o pdbqt/${target_dir}_ligand.pdbqt
+
+    rm $ligand # Remove the symbolic link.
+done
--- 02_dock.sh
+++ 02_dock.sh
@@ -1,6 +1,12 @@
 #!/bin/bash
-vina --receptor pdbqt/receptor.pdbqt \
-     --ligand   pdbqt/ligand.pdbqt \
-     --config receptor_vina_box.txt \
-     --exhaustiveness=32 \
-     --out vina_out_ligand.pdbqt
+
+target_numbers=`ls inputs`
+echo "Target No: " $target_numbers
+for n in $target_numbers; do
+     
+    vina --receptor pdbqt/${n}_receptor.pdbqt \
+         --ligand   pdbqt/${n}_ligand.pdbqt \
+         --config   receptor_vina_box.txt \
+         --exhaustiveness=32 \
+         --out results/${n}_vina_out_ligand.pdbqt
+ done
